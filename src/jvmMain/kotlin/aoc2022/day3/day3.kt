ec25c0dd2ec525f09b93ea0f829ffb41a2bd7d6c
Mike-AMD <mikew670@gmail.com>
2022-12-12 06:50:51 -0600

    day3

--- src/jvmMain/kotlin/aoc2022/day3/day3.kt
+++ src/jvmMain/kotlin/aoc2022/day3/day3.kt
@@ -1,2 +1,34 @@
 package aoc2022.day3
 
+import java.io.File
+
+val actualList = File("C:\\Users\\Mike-AMD\\IntelliJIDEAProjects\\AOC\\src\\jvmMain\\resources\\day3.txt").bufferedReader().readLines()
+
+fun main(args: Array<String>) {
+
+    println(actualList.asSequence().map {
+        val dup = findDup(it)
+        val priority = if (dup.isLowerCase()) {
+            dup - 'a' + 1
+        } else {
+            dup - 'A' + 27
+        }
+        println(priority)
+        priority
+    }.sum())
+}
+
+fun findDup(s: String): Char {
+    val halfPoint = s.length / 2
+
+    val partOne = mutableSetOf<Char>()
+    for (i in 0 until halfPoint) {
+        partOne.add(s[i])
+    }
+
+    for (i in halfPoint until s.length) {
+        if (partOne.contains(s[i])) return s[i]
+    }
+    throw IllegalArgumentException("given sack does not have a duplicate")
+}
+
